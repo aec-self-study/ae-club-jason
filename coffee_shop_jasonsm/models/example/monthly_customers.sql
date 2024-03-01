56c9c0a7f0104026fd2c1e71acbcc9afdc221ecf
jlsmith025 <160658603+jlsmith025@users.noreply.github.com>
2024-03-01 20:46:10 +0000

    fixed function

--- coffee_shop_jasonsm/models/example/monthly_customers.sql
+++ coffee_shop_jasonsm/models/example/monthly_customers.sql
@@ -1,5 +1,5 @@
 select
-    data_trunc(first_order_at, month) as date_month,
+    date_trunc(first_order_at, month) as date_month,
     count(*) as new_customers
 from {{ ref ('customers') }}
 group by date_month
